39dcc90e31057b8cafe7cd45559b58e6e1ab3b57
Dorin David <70648503+Dorin-David@users.noreply.github.com>
2021-05-28 13:48:25 +0200

    fix bugs

--- js/populateQuestion.js
+++ js/populateQuestion.js
@@ -17,26 +17,21 @@ function populateQuestion(question, callback) {
     hljs.highlightAll();
 
     const questionAnswers = Object.keys(answers);
-
     const shuffledAnswers = shuffleAnswers(answers);
-
 
     //populate answers
     questionAnswers.forEach((answer, index) => {
         const { formatedAnswer, rawAnswer } = shuffledAnswers[index];
         const div = document.createElement('div');
-       
-
         div.innerHTML = formatedAnswer
         div.classList.add('answer', answer);
         div.onclick = function () {
-            
             if (rawAnswer === correct) {
                 this.classList.add('correct')
-                callback(true);       
+                callback(true);
             } else {
                 this.classList.add('incorrect')
-                callback(false)       
+                callback(false)
             }
             [...document.querySelectorAll('.answer')].forEach(answer => {
                 answer.onclick = null
@@ -45,7 +40,6 @@ function populateQuestion(question, callback) {
         }
         options.append((div))
     })
-
 
 }
 
