352d80e0ff3f539d7939dba5b795559c586a750c
Dorin David <70648503+Dorin-David@users.noreply.github.com>
2021-06-14 17:28:51 +0200

    finalize theme options + navigation buttons

--- js/selectStyle.js
+++ js/selectStyle.js
@@ -1,31 +1,34 @@
 import themes from './themes/themes.js';
+
 const styleOptionsWrapper = document.querySelector('.settings-themes');
-
-let theme = document.createElement('div');
-const themeImage = document.createElement('img');
-
-{/* <div class="theme-wrapper">
-<img src="./public/assets/atom-one-dark.JPG" alt="a11y-dark_theme">
-<h3>Atom One Dark</h3>
-</div> */}
+const currentStyle = document.querySelector('#code-style');
 
 
-for(let [theme, path] of Object.entries(themes)) {
-    const wrapper = document.createElement('div');
-    const img = document.createElement('img');
-    const themeName = document.createElement('h3');
+function selectTheme(){
+    for(let [theme, path] of Object.entries(themes)) {
+        const wrapper = document.createElement('div');
+        const img = document.createElement('img');
+        const themeName = document.createElement('h3');
+        
+        img.src = `../public/assets/${path}.JPG`;
+        img.alt = theme + ' theme';
+        themeName.textContent = theme;
     
-    img.src = `../public/assets/${path}.JPG`;
-    img.alt = theme + ' theme';
-    themeName.textContent = theme;
-
-    wrapper.classList.add('theme-wrapper')
-    wrapper.append(img, themeName);
-    styleOptionsWrapper.append(wrapper)
-
+        wrapper.classList.add('theme-wrapper')
+        wrapper.append(img, themeName);
+        styleOptionsWrapper.append(wrapper)
+    
+        wrapper.addEventListener('click', function(){
+            const options = document.querySelectorAll('.theme-wrapper');
+    
+            [...options].forEach(option => option.classList.remove('selected'));
+            wrapper.classList.add('selected');
+            currentStyle.href = `./code_styles/${path}.css`
+        })
+    
+    }
+    
+    hljs.highlightAll();
 }
 
-
-
-
-hljs.highlightAll();
+export default selectTheme
