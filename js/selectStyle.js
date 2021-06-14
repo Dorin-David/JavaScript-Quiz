6be244689d46152b3d726ffdc8f9777a2adb699e
Dorin David <70648503+Dorin-David@users.noreply.github.com>
2021-06-14 08:41:12 +0200

    styles

---
+++ js/selectStyle.js
@@ -0,0 +1,16 @@
+const styles = {
+    'default': "an-old-hope.css",
+    'A 11 Y Dark': 'a11y-dark.css',
+    'A 11 Y Light': 'a11y-light.css',
+    'Atom One Dark': 'atom-one-dark.css',
+    'Atom One Dark Reasonable': 'atom-one-dark-reasonable.css',
+    'Atelier Estuary': "atelier-estuary.css",
+    'Brewer': "color-brewer.css",
+    'Darcula': "darcula.css",
+    'Dracula': 'dracula.css',
+    'Github': 'github.css'
+    
+
+}
+
+const styleOptionsWrapper = document.querySelector('.style-options-grid')
