e274a2b2945f3af0aa4c1c56a2bc6db29198a0e8
Dorin David <70648503+Dorin-David@users.noreply.github.com>
2021-06-14 14:09:17 +0200

    themes options style

--- js/selectStyle.js
+++ js/selectStyle.js
@@ -1,5 +1,31 @@
 import themes from './themes/themes.js';
+const styleOptionsWrapper = document.querySelector('.settings-themes');
 
-const styleOptionsWrapper = document.querySelector('.style-options-grid');
+let theme = document.createElement('div');
+const themeImage = document.createElement('img');
+
+{/* <div class="theme-wrapper">
+<img src="./public/assets/atom-one-dark.JPG" alt="a11y-dark_theme">
+<h3>Atom One Dark</h3>
+</div> */}
+
+
+for(let [theme, path] of Object.entries(themes)) {
+    const wrapper = document.createElement('div');
+    const img = document.createElement('img');
+    const themeName = document.createElement('h3');
+    
+    img.src = `../public/assets/${path}.JPG`;
+    img.alt = theme + ' theme';
+    themeName.textContent = theme;
+
+    wrapper.classList.add('theme-wrapper')
+    wrapper.append(img, themeName);
+    styleOptionsWrapper.append(wrapper)
+
+}
+
+
+
 
 hljs.highlightAll();
