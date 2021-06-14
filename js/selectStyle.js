65487d574b0ae62b9dc46ac2b2406bbc3dad7894
Dorin David <70648503+Dorin-David@users.noreply.github.com>
2021-06-14 12:17:20 +0200

    add themes

--- js/selectStyle.js
+++ js/selectStyle.js
@@ -1,16 +1,5 @@
-const styles = {
-    'default': "an-old-hope.css",
-    'A 11 Y Dark': 'a11y-dark.css',
-    'A 11 Y Light': 'a11y-light.css',
-    'Atom One Dark': 'atom-one-dark.css',
-    'Atom One Dark Reasonable': 'atom-one-dark-reasonable.css',
-    'Atelier Estuary': "atelier-estuary.css",
-    'Brewer': "color-brewer.css",
-    'Darcula': "darcula.css",
-    'Dracula': 'dracula.css',
-    'Github': 'github.css'
-    
+import themes from './themes/themes.js';
 
-}
+const styleOptionsWrapper = document.querySelector('.style-options-grid');
 
-const styleOptionsWrapper = document.querySelector('.style-options-grid')
+hljs.highlightAll();
